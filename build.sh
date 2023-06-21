e58231b72602c728e10c266711ec8fc7ba55a772
leone <791960492@qq.com>
2023-06-21 14:33:33 +0800

    update

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ done
 
 ROOT_DIR=$(realpath -s $(dirname $0))
 SGXSAN_DIR=$(realpath -s ${ROOT_DIR}/../../install)
-MAKE_FLAGS="CC=clang-13 CXX=clang++-13 SGX_SDK=${SGXSAN_DIR}"
+MAKE_FLAGS="CC=clang-13 CXX=clang++-13 LD=lld SGX_SDK=${SGXSAN_DIR} -j1"
 CMAKE_FLAGS=
 MODE=${MODE:="RELEASE"}
 FUZZER=${FUZZER:="LIBFUZZER"}
